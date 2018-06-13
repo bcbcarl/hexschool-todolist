bd41cddda4b4c5508228058c8e6a9f6fa3f06d33
Carl X. Su <bcbcarl@gmail.com>
2018-06-13 21:07:58 +0800

    Rename fetchTodosEpic to restoreTodosEpic

---
+++ src/data/todos.js
@@ -0,0 +1,52 @@
+export default [
+  {
+    id: 1528578226000,
+    created_at: 1528578226000,
+    due_at: 1557763200000,
+    title: 'item 2',
+    pinned: true,
+    completed: false,
+    attachment: true,
+    comment: 'meet him at Lorence Cafe'
+  },
+  {
+    id: 1528578231000,
+    created_at: 1528578231000,
+    due_at: 0,
+    title: 'item 4',
+    pinned: true,
+    completed: false,
+    attachment: true,
+    comment: 'meet her at Coffee Shop'
+  },
+  {
+    id: 1528578236000,
+    created_at: 1528578236000,
+    due_at: 1560787200000,
+    title: 'item 5',
+    pinned: false,
+    completed: false,
+    attachment: false,
+    comment: ''
+  },
+  {
+    id: 1528578241000,
+    created_at: 1528578241000,
+    due_at: 0,
+    title: 'item 1',
+    pinned: false,
+    completed: false,
+    attachment: true,
+    comment: ''
+  },
+  {
+    id: 1528578246000,
+    created_at: 1528578246000,
+    due_at: 0,
+    title: 'item 3',
+    pinned: false,
+    completed: true,
+    attachment: false,
+    comment: ''
+  }
+];
--- src/epics/index.js
+++ src/epics/index.js
@@ -4,59 +4,7 @@ import { mergeMap, map, delay } from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
 
 import { increment, fetchTodosFulfilled, fetchUserFulfilled } from '../actions';
-
-const fakeTodos = [
-  {
-    id: 1528578226000,
-    created_at: 1528578226000,
-    due_at: 1557763200000,
-    title: 'item 2',
-    pinned: true,
-    completed: false,
-    attachment: true,
-    comment: 'meet him at Lorence Cafe'
-  },
-  {
-    id: 1528578231000,
-    created_at: 1528578231000,
-    due_at: 0,
-    title: 'item 4',
-    pinned: true,
-    completed: false,
-    attachment: true,
-    comment: 'meet her at Coffee Shop'
-  },
-  {
-    id: 1528578236000,
-    created_at: 1528578236000,
-    due_at: 1560787200000,
-    title: 'item 5',
-    pinned: false,
-    completed: false,
-    attachment: false,
-    comment: ''
-  },
-  {
-    id: 1528578241000,
-    created_at: 1528578241000,
-    due_at: 0,
-    title: 'item 1',
-    pinned: false,
-    completed: false,
-    attachment: true,
-    comment: ''
-  },
-  {
-    id: 1528578246000,
-    created_at: 1528578246000,
-    due_at: 0,
-    title: 'item 3',
-    pinned: false,
-    completed: true,
-    attachment: false,
-    comment: ''
-  }
-];
+import fakeTodos from '../data/todos';
 
 const fetchUserEpic = action$ =>
   action$.pipe(
@@ -68,7 +16,7 @@ const fetchUserEpic = action$ =>
     )
   );
 
-const fetchTodosEpic = action$ =>
+const restoreTodosEpic = action$ =>
   action$.pipe(
     ofType('FETCH_TODOS'),
     mergeMap(() =>
@@ -83,6 +31,6 @@ const incrementEpic = action$ =>
     map(({ payload }) => increment(payload.amount))
   );
 
-const rootEpic = combineEpics(fetchUserEpic, fetchTodosEpic, incrementEpic);
+const rootEpic = combineEpics(fetchUserEpic, restoreTodosEpic, incrementEpic);
 
 export default rootEpic;
